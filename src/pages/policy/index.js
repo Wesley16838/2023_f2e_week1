8e97a72de149c8d6f38ebda4cb70eb942b9272d9
WesleyWong <wesley16838@gmail.com>
2023-12-27 02:15:15 -0600

    policy ui rwd down

--- src/pages/policy/index.js
+++ src/pages/policy/index.js
@@ -1,11 +1,20 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useRef } from 'react';
 import { useRouter } from 'next/router';
 import { FullPage, Slide } from 'react-full-page';
 import useFetch from '@/hooks/useFetch';
 import styles from '@/styles/pages/policy.module.scss'
 import Nav from '@/components/Nav';
 import Footer from '@/components/Footer';
+
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+// core version + navigation, pagination modules:
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useSwiper } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+
 
 class CustomControls extends React.Component {
     static defaultProps = {
@@ -50,22 +59,41 @@ const PolicyPage = () => {
         gridTemplateColumns: '1fr 1fr',
         position: 'relative'
     };
+    const swiperRef = useRef(null);
+    const handleButtonClick = (val) => {
+        if (swiperRef.current) {
+            val == true ? swiperRef.current.slideNext() : swiperRef.current.slidePrev();
+        }
+    };
 
-    return <FullPage controls={CustomControls}>
+    const handleSlideChange = () => {
+
+        if (swiperWrapperRef.current) swiperWrapperRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    return <><div className={styles["policy-picture-container-wrapper-tablet"]}><Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        navigation
+        cssMode={true}
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiperInstance) => {
+            // Assign the swiper instance to the ref
+            swiperRef.current = swiperInstance;
+        }}
+        onSlideChange={handleSlideChange}
+    >
         <Nav data={data} />
         {
             issueData && issueData.map(
                 (issue, index) => {
-                    return <Slide style={{
-                        ...baseStyle,
-                    }} key={issue.name}>
-                        <div className={styles["policy-picture-container"]}>
-                            <Image src={issue.fullAsset} width={0}
-                                height={0} alt={issue.name} layout="fill"
-                                objectFit="cover" />
-                            <Image src="/assets/icons/policy-slogan.svg" width={192} height={211} alt={'policy slogan'} style={{ position: 'absolute', left: 40, bottom: 146, zIndex: 2 }} />
+                    return <SwiperSlide>
+                        <div className={styles["policy-picture-container-tablet"]}><Image src={issue.fullAsset} width={0}
+                            height={0} alt={issue.name} layout="fill"
+                            objectFit="cover" />
+                            <Image src="/assets/icons/policy-slogan-tablet.svg" width={243} height={57} alt={'policy slogan'} style={{ position: 'absolute', left: 24, bottom: 24, zIndex: 2 }} />
                         </div>
-                        <div className={styles["policy-container"]}>
+                        <div className={styles["policy-container-tablet"]}>
                             <h4>{data && data.policypage.policy} {index + 1}</h4>
                             <h2>{issue.name}</h2>
                             <ul>
@@ -75,17 +103,52 @@ const PolicyPage = () => {
                                     })
                                 }
                             </ul>
-                            <Image src={issue.backgroundImage.asset} width={issue.backgroundImage.size.width} height={issue.backgroundImage.size.height} alt={'policy slogan'} style={{ position: 'absolute', right: issue.backgroundImage.position.right, top: issue.backgroundImage.position.top }} />
-                        </div>
+                            <div className={styles['policy-button-wrapper']}>{index != 0 ? <button onClick={() => handleButtonClick(false)}>{data && data.policypage.previousPolicy} </button> : <div></div>}{index < issueData.length - 1 && <button onClick={() => handleButtonClick(true)}>{data && data.policypage.nextPolicy} <div className={styles['button-icon-image']}><Image src="/assets/icons/blue-arrow-right.svg" width={16} height={16} alt={'next policy'} /></div></button>}</div>
 
-                    </Slide>
-                }
-            )
+                        </div>
+                    </SwiperSlide>
+                })
         }
-        <div className={styles["footer-container"]}>
-            <Footer data={data} />
-        </div>
-    </FullPage>
+        <Footer data={data} />
+    </Swiper>
+    </div>
+        <div className={styles["policy-picture-container-wrapper"]}><FullPage controls={CustomControls}>
+            <Nav data={data} />
+            {
+                issueData && issueData.map(
+                    (issue, index) => {
+                        return <Slide style={{
+                            ...baseStyle,
+                        }} key={issue.name}>
+                            <div className={styles["policy-picture-container"]}>
+                                <Image src={issue.fullAsset} width={0}
+                                    height={0} alt={issue.name} layout="fill"
+                                    objectFit="cover" />
+                                <Image src="/assets/icons/policy-slogan.svg" width={192} height={211} alt={'policy slogan'} style={{ position: 'absolute', left: 40, bottom: 146, zIndex: 2 }} />
+                            </div>
+                            <div className={styles["policy-container"]}>
+                                <h4>{data && data.policypage.policy} {index + 1}</h4>
+                                <h2>{issue.name}</h2>
+                                <ul>
+                                    {
+                                        issue.content.map(item => {
+                                            return <div className={styles["list-item"]} key={item}><Image src="/assets/icons/blue-dot.svg" width={12} height={12} alt="blue dot" /><li>{item}</li></div>
+                                        })
+                                    }
+                                </ul>
+                                <Image src={issue.backgroundImage.asset} width={issue.backgroundImage.size.width} height={issue.backgroundImage.size.height} alt={'policy slogan'} style={{ position: 'absolute', right: issue.backgroundImage.position.right, top: issue.backgroundImage.position.top }} />
+                            </div>
+
+                        </Slide>
+                    }
+                )
+            }
+            <div className={styles["footer-container"]}>
+                <Footer data={data} />
+            </div>
+        </FullPage></div>
+    </>
+
 }
 
 export default PolicyPage;
